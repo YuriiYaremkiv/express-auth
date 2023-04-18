07f20ed48a67d3e4fe52abb82978620a3f356681
yyaremkiv <y.yaremkiv@gmail.com>
2023-04-18 14:10:25 +0300

    cnange auth redux

--- client/src/redux/auth/authOperations.js
+++ client/src/redux/auth/authOperations.js
@@ -4,45 +4,51 @@ import AuthService from "../../services/AuthServices";
 class AuthActions {
   static login = createAsyncThunk(
     "auth/login",
-    async ({ email, password }, thunkAPI) => {
+    async ({ email, password }, { rejectWithValue }) => {
       try {
         const { data } = await AuthService.login(email, password);
         return data;
       } catch (err) {
-        return thunkAPI.rejectWithValue(err.message);
+        return rejectWithValue(err.message);
       }
     }
   );
 
   static register = createAsyncThunk(
     "auth/register",
-    async ({ email, password }, thunkAPI) => {
+    async ({ email, password }, { rejectWithValue }) => {
       try {
         const { data } = await AuthService.register(email, password);
         return data;
       } catch (err) {
-        return thunkAPI.rejectWithValue(err.message);
+        return rejectWithValue(err.message);
       }
     }
   );
 
-  static logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
-    try {
-      const { data } = await AuthService.logout();
-      return data;
-    } catch (err) {
-      return thunkAPI.rejectWithValue(err.message);
+  static logout = createAsyncThunk(
+    "auth/logout",
+    async (_, { rejectWithValue }) => {
+      try {
+        const { data } = await AuthService.logout();
+        return data;
+      } catch (err) {
+        return rejectWithValue(err.message);
+      }
     }
-  });
+  );
 
-  static refresh = createAsyncThunk("auth/refresh", async (_, thunkAPI) => {
-    try {
-      const { data } = await AuthService.refresh();
-      return data;
-    } catch (err) {
-      return thunkAPI.rejectWithValue(err.message);
+  static refresh = createAsyncThunk(
+    "auth/refresh",
+    async (_, { rejectWithValue }) => {
+      try {
+        const { data } = await AuthService.refresh();
+        return data;
+      } catch (err) {
+        return rejectWithValue(err.message);
+      }
     }
-  });
+  );
 }
 
 export default AuthActions;
